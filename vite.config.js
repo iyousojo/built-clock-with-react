8ab0a379eb7ab9273ccb92f5525a94e0e58fdba4
solomon <solomonjohnbull676@gmail.com>
2025-06-21 02:54:46 +0100

    Remove unused imports from vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,5 @@ export default defineConfig({
   plugins: [react()],
    base: '/built-clock-with-react.git/', // Replace <your-repo-name> with your actual repository name
 })
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+
 
